a7d6601e45d4402bf40e43035a3b333aa5250c12
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2024-06-27 15:22:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ script/bottle.js
@@ -0,0 +1,149 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { bottle, bottleMobile } from "./transform";
+import gsap from "gsap";
+import "../style.css";
+
+// Инициализация и создание сцены и 3д модели
+const canvas = document.querySelector("canvas.webgl");
+let objectPosition = window.innerWidth < 600 ? bottleMobile : bottle;
+const modelThree = "../bottle.glb";
+// Scene
+const scene = new THREE.Scene();
+// Sizes
+// Размеры камеры
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+window.addEventListener("resize", function () {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+});
+
+// GLTF Loader
+let pokeball = null;
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load(
+  modelThree,
+  (gltf) => {
+    pokeball = gltf.scene;
+    pokeball.rotation.y = objectPosition[0].rotationY;
+    pokeball.rotation.x = objectPosition[0].rotationX;
+    pokeball.rotation.z = objectPosition[0].rotationZ;
+    pokeball.position.x = objectPosition[0].positionX;
+    pokeball.position.y = objectPosition[0].positionY;
+    pokeball.position.z = 3;
+
+    const radius = 0.5;
+    pokeball.scale.set(radius, radius, radius);
+    scene.add(pokeball);
+  },
+  (xhr) => {
+    // Этот код будет выполнен
+    // в процессе загрузки
+    const totalSized = 28955816; // При каждой новой модели занести количество байтов
+    // console.log(xhr.loaded, xhr.total, xhr.loaded / totalSized);
+    let preload = (xhr.loaded / totalSized) * 100 + "";
+    let preloadSplit = preload.split(".");
+    const loaderDiv = document.querySelector(".loader");
+    loaderDiv.innerHTML = `
+    <div class="lineLoader">
+      <div class="inLineLoader" style="width: ${preloadSplit[0]}%"></div>
+    </div>
+    <h3>
+      Пожалуйста подождите <br />
+      Идет загрузка <br />
+      ${preloadSplit[0]}%
+    </h3>
+    `;
+    if (preloadSplit[0] == "100") {
+      loaderDiv.classList.add("noActive");
+    }
+  },
+  (error) => {
+    console.error(error);
+  }
+);
+
+// Camera
+// Настройка камеры для десктопа и мобильных устройств
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+camera.position.y = 0.5;
+
+scene.add(camera);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+  alpha: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.render(scene, camera);
+// Scroll
+// Координаты для секций
+
+// Анимация и формула нахождения секции
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener("scroll", () => {
+  scrollY = window.scrollY;
+  const newSection = Math.round(scrollY / sizes.height);
+
+  if (newSection != currentSection) {
+    currentSection = newSection;
+
+    if (!!pokeball) {
+      gsap.to(pokeball.rotation, {
+        duration: 4.5,
+        ease: "power2.inOut",
+        y: objectPosition[currentSection].rotationY,
+        x: objectPosition[currentSection].rotationX,
+        z: objectPosition[currentSection].rotationZ,
+      });
+      gsap.to(pokeball.position, {
+        duration: 4.5,
+        ease: "power2.inOut",
+        x: objectPosition[currentSection].positionX,
+        y: objectPosition[currentSection].positionY,
+      });
+      gsap.to(pokeball.scale, {
+        duration: 4.5,
+        ease: "power2.inOut",
+        ...objectPosition[currentSection].scale,
+      });
+    }
+  }
+});
+
+// Light
+// Настройка света
+const ambientLight = new THREE.AmbientLight(0xffffff, 5.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+directionalLight.position.set(1, 2, 0);
+scene.add(directionalLight);
+
+// Animate
+const clock = new THREE.Clock();
+let lastElapsetTime = 0;
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - lastElapsetTime;
+  lastElapsetTime = elapsedTime;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
---
+++ script/transform.js
@@ -0,0 +1,354 @@
+export const costume = [
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: -2.9,
+    rotationZ: 0,
+    positionX: -0.4,
+    positionY: 0,
+  },
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: (10 * Math.PI) / 180,
+    rotationZ: 0,
+    positionX: 0.3,
+    positionY: 0,
+  },
+  {
+    scale: { x: 2, y: 2, z: 2 },
+    rotationY: -2.7,
+    rotationZ: 0,
+    positionX: -0.5,
+    positionY: -2.3,
+  },
+  {
+    scale: { x: 2, y: 2, z: 2 },
+    rotationY: -2.7,
+    rotationZ: 0,
+    positionX: 0.8,
+    positionY: -2,
+  },
+  {
+    scale: { x: 2.5, y: 2.5, z: 2.5 },
+    rotationY: -3,
+    rotationZ: 0,
+    positionX: -0.5,
+    positionY: -4,
+  },
+];
+export const costumeMobile = [
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: -2.9,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0,
+  },
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: (10 * Math.PI) / 180,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0,
+  },
+  {
+    scale: { x: 2, y: 2, z: 2 },
+    rotationY: -2.7,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: -2.3,
+  },
+  {
+    scale: { x: 1.6, y: 1.6, z: 1.6 },
+    rotationY: -2.7,
+    rotationZ: 0,
+    positionX: 0.5,
+    positionY: -1.5,
+  },
+  {
+    scale: { x: 2.5, y: 2.5, z: 2.5 },
+    rotationY: -3,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: -3.9,
+  },
+];
+
+export const bottle = [
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: 0.5,
+    rotationX: 0.1,
+    rotationZ: 0,
+    positionX: -0.4,
+    positionY: 0.3,
+  },
+  {
+    scale: { x: 1, y: 1, z: 1 },
+    rotationY: 3,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0.3,
+    positionY: 0.1,
+  },
+  {
+    scale: { x: 2, y: 2, z: 2 },
+    rotationY: -2.7,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: -0.5,
+    positionY: -1.1,
+  },
+  {
+    scale: { x: 1.7, y: 1.7, z: 1.7 },
+    rotationY: 2.7,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0.4,
+    positionY: 0.2,
+  },
+];
+
+export const bottleMobile = [
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: 0.5,
+    rotationX: 0.1,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0.4,
+  },
+  {
+    scale: { x: 0.7, y: 0.7, z: 0.7 },
+    rotationY: 3,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0.3,
+  },
+  {
+    scale: { x: 2, y: 2, z: 2 },
+    rotationY: -2.7,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: -0.9,
+  },
+  {
+    scale: { x: 1.7, y: 1.7, z: 1.7 },
+    rotationY: 2.7,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0.2,
+  },
+];
+
+export const dagger = [
+  {
+    scale: { x: 0.15, y: 0.15, z: 0.15 },
+    rotationY: 0,
+    rotationX: 0,
+    rotationZ: 1.2,
+    positionX: -0.4,
+    positionY: 0.6,
+  },
+  {
+    scale: { x: 0.12, y: 0.12, z: 0.12 },
+    rotationY: 6,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0.3,
+    positionY: 0.6,
+  },
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: -0.2,
+    rotationX: 0,
+    rotationZ: 1.2,
+    positionX: -1,
+    positionY: 0.6,
+  },
+  {
+    scale: { x: 0.2, y: 0.2, z: 0.2 },
+    rotationY: 6,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0.4,
+    positionY: 0.1,
+  },
+];
+
+export const daggerMobile = [
+  {
+    scale: { x: 0.1, y: 0.1, z: 0.1 },
+    rotationY: 0,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0.6,
+  },
+  {
+    scale: { x: 0.15, y: 0.15, z: 0.15 },
+    rotationY: 6,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0.9,
+  },
+  {
+    scale: { x: 0.7, y: 0.7, z: 0.7 },
+    rotationY: -0.2,
+    rotationX: 0,
+    rotationZ: 1.2,
+    positionX: -0.3,
+    positionY: 0.8,
+  },
+  {
+    scale: { x: 0.2, y: 0.2, z: 0.2 },
+    rotationY: 6,
+    rotationX: 0,
+    rotationZ: 0,
+    positionX: 0,
+    positionY: 0.2,
+  },
+];
+
+export const jewerly = [
+  {
+    scale: { x: 0.1, y: 0.1, z: 0.1 },
+    rotationY: 0,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.6,
+    positionX: -0.4,
+  },
+  {
+    scale: { x: 0.12, y: 0.12, z: 0.12 },
+    rotationY: 2,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.6,
+    positionX: 0.3,
+  },
+  {
+    scale: { x: 0.17, y: 0.17, z: 0.17 },
+    rotationY: 4,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 1.2,
+    positionX: 0,
+  },
+  {
+    scale: { x: 0.1, y: 0.1, z: 0.1 },
+    rotationY: -2.3,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.6,
+    positionX: 0.4,
+  },
+];
+
+export const jewerlyMobile = [
+  {
+    scale: { x: 0.05, y: 0.05, z: 0.05 },
+    rotationY: 0,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.6,
+    positionX: 0,
+  },
+  {
+    scale: { x: 0.05, y: 0.05, z: 0.05 },
+    rotationY: 1,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.7,
+    positionX: 0,
+  },
+  {
+    scale: { x: 0.1, y: 0.1, z: 0.1 },
+    rotationY: 4,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 1.1,
+    positionX: 0.2,
+  },
+  {
+    scale: { x: 0.05, y: 0.05, z: 0.05 },
+    rotationY: -2.3,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.6,
+    positionX: 0,
+  },
+];
+
+export const kibitka = [
+  {
+    scale: { x: 0.3, y: 0.3, z: 0.3 },
+    rotationY: 0.5,
+    rotationX: 0.3,
+    rotationZ: 0,
+    positionY: 0.4,
+    positionX: -0.4,
+  },
+  {
+    scale: { x: 0.3, y: 0.3, z: 0.3 },
+    rotationY: 0,
+    rotationX: 1.3,
+    rotationZ: 0,
+    positionY: 0.4,
+    positionX: 0.3,
+  },
+  {
+    scale: { x: 0.5, y: 0.5, z: 0.5 },
+    rotationY: 0.2,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.3,
+    positionX: -0.4,
+  },
+  {
+    scale: { x: 0.7, y: 0.7, z: 0.7 },
+    rotationY: -0.8,
+    rotationX: 0,
+    rotationZ: 0,
+    positionY: 0.2,
+    positionX: 0.4,
+  },
+];
+
+export const kibitkaMobile = [
+  {
+    scale: { x: 0.19, y: 0.19, z: 0.19 },
+    rotationY: 0.5,
+    rotationX: 0.3,
+    rotationZ: 0,
+    positionY: 0.5,
+    positionX: 0,
+  },
+  {
+    scale: { x: 0.19, y: 0.19, z: 0.19 },
+    rotationY: 0,
+    rotationX: 1.3,
+    rotationZ: 0,
+    positionY: 0.55,
+    positionX: 0,
+  },
+  {
+    scale: { x: 0.4, y: 0.4, z: 0.4 },
+    rotationY: 0,
+    rotationX: 0.2,
+    rotationZ: 0,
+    positionY: 0.6,
+    positionX: 0,
+  },
+  {
+    scale: { x: 0.4, y: 0.4, z: 0.4 },
+    rotationY: -0.4,
+    rotationX: 0.2,
+    rotationZ: 0,
+    positionY: 0.5,
+    positionX: 0,
+  },
+];
